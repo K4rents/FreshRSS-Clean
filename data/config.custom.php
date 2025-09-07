08940e2ea05cd573d6535c9731e31711bdfede84
K4rents <karensclothes41@gmail.com>
2025-09-07 03:24:09 -0600

    config.custom.php

---
+++ data/config.custom.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    // Esto fuerza a FreshRSS a usar la raíz del dominio y no /p/
+    'base_url' => '/',
+
+    // Opcional: lenguaje de la interfaz
+    'language' => 'en',
+
+    // Opcional: título que quieres que se vea
+    'title' => 'FreshRSS-Clean Render',
+];
