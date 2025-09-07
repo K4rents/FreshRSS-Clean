330f9a5e2ffea29f2cb27045d5fff23f3c03623b
K4rents <karensclothes41@gmail.com>
2025-09-07 13:03:16 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
 declare(strict_types=1);
 
+// Cargar constantes primero
+require(__DIR__ . '/constants.php');
+
+// Librerías base
 require(__DIR__ . '/lib/lib_rss.php');
+
+// Núcleo de la aplicación
 require(__DIR__ . '/app/FreshRSS.php');
 
+// Inicializar FreshRSS
 FreshRSS::preInitialize();
 FreshRSS::initialize();
 FreshRSS::run();
