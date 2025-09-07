6f09976e85c34c991c9c1e5e3a2dfdd601eb5a60
K4rents <karensclothes41@gmail.com>
2025-09-07 04:06:39 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
 declare(strict_types=1);
 
-// Configuración de FreshRSS
-$configFile = __DIR__ . '/data/config.custom.php';
-if (file_exists($configFile)) {
-    require $configFile;
-}
-
-// Cargar la aplicación FreshRSS
-require __DIR__ . '/index.php';
+// Cargar la aplicación FreshRSS desde la carpeta 'p'
+require __DIR__ . '/p/index.php';
