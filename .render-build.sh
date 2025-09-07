a15643e3a61cb4401843d72d06de07e1a373bdd3
K4rents <karensclothes41@gmail.com>
2025-09-07 04:55:19 -0600

    Update .render-build.sh

--- .render-build.sh
+++ .render-build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
-echo "🧹 Limpiando archivos temporales innecesarios..."
-# Si quieres conservar tu código, NO borres /var/www/html/*
-# Solo limpia caches u otras carpetas si es necesario
+echo "🧹 Limpiando carpeta /var/www/html..."
+rm -rf /var/www/html/*
+
+echo "📦 Copiando archivos al contenedor..."
+cp -r . /var/www/html/
 
 echo "🔑 Ajustando permisos..."
 chown -R www-data:www-data /var/www/html/
