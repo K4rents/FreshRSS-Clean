acd247a82b03bba4b750344d1a07685408bd1348
K4rents <karensclothes41@gmail.com>
2025-09-07 17:48:56 -0600

    Update .render-build.sh

--- .render-build.sh
+++ .render-build.sh
@@ -9,3 +9,12 @@ cp -r . /var/www/html/
 
 echo "🔑 Ajustando permisos..."
 chown -R www-data:www-data /var/www/html/
+
+# Crear carpetas críticas dentro de data
+mkdir -p /var/www/html/data/users
+mkdir -p /var/www/html/data/logs
+mkdir -p /var/www/html/data/tmp
+
+# Ajustar permisos
+chmod -R 777 /var/www/html/data/users /var/www/html/data/logs /var/www/html/data/tmp
+chown -R www-data:www-data /var/www/html/data
