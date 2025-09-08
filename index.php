ef655f681d0bf1a4a65a8012f3e030c0fb422e9b
K4rents <karensclothes41@gmail.com>
2025-09-07 18:47:02 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 // Cargar constantes
 require(__DIR__ . '/constants.php');
 
-// Cargar librerías
+// Cargar librerías y núcleo
 require(__DIR__ . '/lib/lib_rss.php');
 require(__DIR__ . '/app/FreshRSS.php');
 
-// Inicializar la aplicación
+// Iniciar la aplicación
 $controller = new Minz_FrontController();
 $controller->run();
